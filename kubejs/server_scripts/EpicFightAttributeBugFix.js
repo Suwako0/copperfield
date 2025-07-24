33e4c6d46e24f8d6d4625ff08baf874280b8b85b
Suwako <1462579982@qq.com>
2025-07-25 03:32:21 +0800

    simple update

--- kubejs/server_scripts/EpicFightAttributeBugFix.js
+++ kubejs/server_scripts/EpicFightAttributeBugFix.js
@@ -1,26 +1,29 @@
 EntityEvents.hurt(event => {
+    // 由于怪物互殴也会崩溃，这里就不判断伤害来源是不是玩家了
     if (event.entity.type !== 'minecraft:player') {
         event.entity.attributes.getSyncableAttributes().forEach(instance => {
             const modifiers = instance.modifiers;
             modifiers.forEach(modifier => {
                 //event.server.runCommand(`/say modifier name:${modifier.getName()}`);
-                //硬编码修复傻逼史诗战斗崩溃
+                // 硬编码修复傻逼史诗战斗崩溃 移除史诗战斗重复添加的modifier
                 if (modifier.getName()=== 'Armor modifier' || modifier.getName()=== 'epicfight:weapon_modifier' || modifier.getName()=== 'Weapon modifier') {
                     //event.server.runCommand(`/say delete modifier:${modifier.getName()}`);
                     instance.removeModifier(modifier.getId())
                 }
-                //instance.removeModifier(modifier.getId())
             })
         })
     }
 })
 
 EntityEvents.death(event => {
-    if (event.entity.type === 'minecraft:zombie') {
+    // 实体死亡的时候也兜底一下
+    if (event.entity.type !== 'minecraft:player') {
         event.entity.attributes.getSyncableAttributes().forEach(instance => {
             const modifiers = instance.modifiers;
             modifiers.forEach(modifier => {
-                instance.removeModifier(modifier.getId())
+                if (modifier.getName()=== 'Armor modifier' || modifier.getName()=== 'epicfight:weapon_modifier' || modifier.getName()=== 'Weapon modifier') {
+                    instance.removeModifier(modifier.getId())
+                }
             })
         })
     }
